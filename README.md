10aa4f22fadda5de1962aae6ee0f7dd1cea6532b
John Doe <johndoe@example.com>
2021-09-24 14:20:50 -0700

    revert all changes since first commmit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # hello
 For PSYCH 251
-
-## This is an open repository for *PSYCH 251*
-* Week 1: Git + GitHub Tutorial
--- pset0.R
+++
@@ -1,7 +0,0 @@
-######################
-##### JIWON BYUN #####
-#### Sep 24, 2021 ####
-######################
-
-print("hello world!")
-print("some changes to the file")
